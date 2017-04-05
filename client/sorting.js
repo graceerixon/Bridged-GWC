6056980a3aaab768ef94db942a005d27442886f6
g_r_a_c_e <serixog865@ops.org>
2017-04-04 23:25:55 -0500

    styling

---
+++ client/sorting.js
@@ -0,0 +1,26 @@
+function calculateSimilarity(teenInterests, seniorInterests) {
+	var score = 0;
+	for (var i = 0; i < seniorInterests.length; i++) {
+		var point = teenInterests.indexOf(seniorInterests[i]);
+		if (point == -1)
+			score += 0;
+		else
+			score += 1;
+	}
+	return score;
+};
+
+export function assignment(teens, seniors, amount) {
+	var groups = [];
+	var num = seniors.length;
+	
+	// do I have a functional array of the seniors and teens?
+	
+	//run through loop for the number of seniors to calculate their score with each teen
+	
+	//find the highest score
+	
+	//return an array of that senior with their paired teen
+	
+	//remove that senior from the array seniors
+}
