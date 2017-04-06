27cd41333fc4fd903f96da7114b80e334a21bbd1
g_r_a_c_e <serixog865@ops.org>
2017-04-05 19:31:18 -0500

    finalized forms

--- client/seniorForm.js
+++ client/seniorForm.js
@@ -26,5 +26,59 @@ Template.seniorForm.events({
 	console.log(event);
 	Meteor.call('insert seniorInfo', email, seniorFirst, seniorLast, seniorInterests, place, entrance);
 	FlowRouter.go('home');
+	},
+	
+		'click #firstBox':function selectMeOne() {
+		document.getElementById("firstBox").checked = !document.getElementById("firstBox").checked;
+		var e = document.getElementById("firstBox");
+		if (e.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Communication,";
+		}
+  },
+  
+  'click #secondBox': function selectMeTwo() {
+		document.getElementById("secondBox").checked = !document.getElementById("secondBox").checked;
+		var e = document.getElementById("secondBox");
+		if (e.checked) {
+			var already2 = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already2 + "  The Internet,";
+		}
+	},
+	
+	'click #thirdBox': function selectMeThree() {
+		document.getElementById("thirdBox").checked = !document.getElementById("thirdBox").checked;
+		var e = document.getElementById("thirdBox");
+		if (e.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Mobile Devices,";
+		}
+	},
+	
+	'click #fourBox': function selectMeFour() {
+		document.getElementById("fourBox").checked = !document.getElementById("fourBox").checked;
+		var e = document.getElementById("fourBox");
+		if (e.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Social Media,";
+		}
+	},
+	
+	'click #fiveBox': function selectMeFive() {
+		document.getElementById("fiveBox").checked = !document.getElementById("fiveBox").checked;
+		var e = document.getElementById("fiveBox");
+		if (e.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Computers,";
+		}
+	},
+	
+	'click #sixBox': function selectMeSix() {
+		document.getElementById("sixBox").checked = !document.getElementById("sixBox").checked;
+		var e = document.getElementById("sixBox");
+		if (e.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Word Processing,";
+		}
 	}
 });
--- client/teenForm.js
+++ client/teenForm.js
@@ -1,7 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { selectMeOne } from './boxes.js';
 
 import './main.html';
 
@@ -32,4 +31,58 @@ Template.teenForm.events({
 	Meteor.call('insert teenInfo', email, firstName, lastName, interests, phone, zip, age, school, answer);
 	FlowRouter.go('home');
 	},
-  });
+	
+	'click #firstBox':function selectMeOne() {
+		document.getElementById("firstBox").checked = !document.getElementById("firstBox").checked;
+		var e = document.getElementById("firstBox");
+		if (e.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Communication,";
+		}
+  },
+  
+  'click #secondBox': function selectMeTwo() {
+		document.getElementById("secondBox").checked = !document.getElementById("secondBox").checked;
+		var f = document.getElementById("secondBox");
+		if (f.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "  The Internet,";
+		}
+	},
+	
+	'click #thirdBox': function selectMeThree() {
+		document.getElementById("thirdBox").checked = !document.getElementById("thirdBox").checked;
+		var g = document.getElementById("thirdBox");
+		if (g.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Mobile Devices,";
+		}
+	},
+	
+	'click #fourBox': function selectMeFour() {
+		document.getElementById("fourBox").checked = !document.getElementById("fourBox").checked;
+		var h = document.getElementById("fourBox");
+		if (h.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Social Media,";
+		}
+	},
+	
+	'click #fiveBox': function selectMeFive() {
+		document.getElementById("fiveBox").checked = !document.getElementById("fiveBox").checked;
+		var i = document.getElementById("fiveBox");
+		if (i.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Computers,";
+		}
+	},
+	
+	'click #sixBox': function selectMeSix() {
+		document.getElementById("sixBox").checked = !document.getElementById("sixBox").checked;
+		var j = document.getElementById("sixBox");
+		if (j.checked) {
+			var already = document.getElementById("display").innerText;
+			document.getElementById("display").innerText = already + "   Word Processing,";
+		}
+	}
+});
